a6f675279e6c932ee0f54c0b96041cea4d6a0d0a
Trevor Starick <trevor.starick@gmail.com>
2014-09-25 02:52:13 -0400

    Removed older controllers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,27 +10,6 @@ angular.module('starter.controllers', [])
   }).then(function(modal) {
     $scope.modal = modal;
   });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
 })
 
 .controller('listView', function($scope) {
